6aaad09029a2c1aa55909c464c8d31209dd2f4bf
Judah Hooper <judahhooper@icloud.com>
2023-04-14 11:37:52 +0100

    prev commit pt2

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -7,7 +7,10 @@ class App extends React.Component {
       <div>
   <h1>Ja<span class="highlight">mmm</span>ing</h1>
   <div class="App">
+    {/* <SearchBar /> */}
     <div class="App-playlist">
+      {/* <SearchResults /> */}
+      {/* <Playlist /> */}
     </div>
   </div>
 </div>
