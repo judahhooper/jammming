cc19b0dca6b810971e47e927a5c4085a903b66f2
Judah Hooper <judahhooper@icloud.com>
2023-04-14 12:23:34 +0100

    missed a bit

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -11,12 +11,11 @@ class Track extends React.Component{
     }
     render() {
         return (
-            <div class="Track">
+            <div className="Track">
   <div className="Track-information">
-    {/* <h3><!-- track name will go here --></h3> */}
-    {/* <p><!-- track artist will go here--> | <!-- track album will go here --></p> */}
+    <h3>track name</h3>
+    <p>artist | album</p>
   </div>
-  {/* <button className="Track-action"><!-- + or - will go here --></button> */}
   {this.renderAction()}
 </div>
         )
