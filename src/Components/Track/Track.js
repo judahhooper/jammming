2dd941ba48822256023ffe290d49a9ff33607890
Judah Hooper <judahhooper@icloud.com>
2023-04-17 11:45:16 +0100

    project steps are broken according to forums

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -13,10 +13,10 @@ class Track extends React.Component{
         return (
             <div className="Track">
   <div className="Track-information">
-    <h3>track name</h3>
-    <p>artist | album</p>
+    <h3>{this.props.track.name}</h3>
+    <p>{this.props.track.artist} | {this.props.track.album}</p> 
   </div>
-  {this.renderAction()}
+  {this.renderAction()} 
 </div>
         )
     }
